54024152a47edbdc175fc9973b0382522db71225
bagoesrex <bagoesrex@gmail.com>
2025-10-02 04:48:09 +0700

    feat(auth): add auth middleware & decorator, implement get user service

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -7,6 +7,7 @@ import { Logger } from "winston";
 import { UserValidation } from "./user.validation";
 import * as bcrypt from 'bcrypt';
 import { v4 as uuid } from 'uuid';
+import { User } from "generated/prisma";
 
 @Injectable()
 export class UserService {
@@ -79,6 +80,13 @@ export class UserService {
             username: user.username,
             name: user.name,
             token: user.token!,
+        }
+    }
+
+    async get(user: User): Promise<UserResponse> {
+        return {
+            username: user.username,
+            name: user.name
         }
     }
 }
