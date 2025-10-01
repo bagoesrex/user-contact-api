8beeca4c9a4230920cd7f587cb333d304bc49f66
bagoesrex <bagoesrex@gmail.com>
2025-10-02 03:20:50 +0700

    feat(user): implement error filter for validation and http errors

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from "@nestjs/common";
+import { Body, Controller, Get, HttpCode, Post } from "@nestjs/common";
 import { UserService } from "./user.service";
 import { WebResponse } from "src/model/web.model";
 import { RegisterUserRequest, UserResponse } from "src/model/user.module";
@@ -8,6 +8,7 @@ export class UserController {
     constructor(private userService: UserService) { }
 
     @Post()
+    @HttpCode(200)
     async register(@Body() request: RegisterUserRequest): Promise<WebResponse<UserResponse>> {
         const result = await this.userService.register(request)
         return {
